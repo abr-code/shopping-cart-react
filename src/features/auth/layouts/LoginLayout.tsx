668dc31b960079f2cd867256433ebbe4fe2f1f4f
abr-code <abraham.dev.contact@gmail.com>
2025-04-01 16:43:51 -0500

    feat: add login layout component

---
+++ src/features/auth/layouts/LoginLayout.tsx
@@ -0,0 +1,11 @@
+import { Login } from "../components/AuthForm";
+import "./LoginLayout.css";
+function LoginLayout() {
+  return (
+    <div className="login_layout-container">
+      <Login />
+    </div>
+  );
+}
+
+export { LoginLayout };
